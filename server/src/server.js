d2a1463274301af373719dae5e8d60b1aecbd5d6
Joseph Daniel H <josephdaniel8805@gmail.com>
2026-05-20 21:18:38 +0530

    Third Commit

---
+++ server/src/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("API running...");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
